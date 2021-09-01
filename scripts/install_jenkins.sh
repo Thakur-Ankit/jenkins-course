10136354c96a5ffb27711c5420426ddd8f900f6e
Ankit <36167987+Thakur-Ankit@users.noreply.github.com>
2021-09-01 15:09:40 +0530

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -3,7 +3,8 @@
 # this script is only tested on ubuntu xenial
 
 # install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
